fdbbceb669bcb0080028a2fd2ed0468537774a9c
Elena Bychenkova <elena.bychenkova@gmail.com>
2022-05-23 19:54:09 -0700

    feat: add alert

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 window.onload = function () {
 	const msg =
 		"Hi! \nI am really sorry I was not able to finish this task on time. \nI had exams at my college past two weeks. \nCould you please check my work later? I would highly appreciate that! Thank you for understanding! \nRegards, \nElena";
-	// alert(msg);
+	alert(msg);
 };
